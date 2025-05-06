8ffe31bb2cf2f29d73dc282d63538a6c6dc01e83
AdityaPandey79 <aditya.pandey@kiwitech.com>
2025-05-06 15:04:03 +0530

    Updated serializer and requirements

--- user/serializers.py
+++ user/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password', 'first_name', 'last_name', 'email']
+        fields = ['id','username', 'password', 'first_name', 'last_name', 'email']
         extra_kwargs = {
             'password': {'write_only': True}
         }
